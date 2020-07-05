39cf8b3bb4767b0ca828ae07ce2b20a07d2f1a3b
Krishna Satya <vadapallikrishna@gmail.com>
2020-07-05 14:26:04 +0530

    commit

--- cmd.js
+++ cmd.js
@@ -1,11 +1,23 @@
-const minimist = require('minimist')
+#! /usr/bin/env node
 
+const minimist = require('minimist')
+const fs = require('fs');
 
 const argv = minimist(process.argv.slice(2))
 const cmd = argv._[0]
 
+function createFile(name) {
+  fs.writeFile("public/" + name);
+}
+
 if(cmd == "build") {
     console.log("building");
+    
+    let jsonFile = fs.readFileSync('raasta.json');
+    console.log(jsonFile);
+    fs.mkdir('public/');
+    	
+    	
 } else if(cmd == "help") {
     console.log("Raasta Build");
     console.log("build - to build project ");
