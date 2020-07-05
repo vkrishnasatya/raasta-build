e580ec3dcb7ba241d2eca8d88bb0e0e6f636ccce
Krishna Satya <vadapallikrishna@gmail.com>
2020-07-05 17:04:12 +0530

    rm

--- cmd.js
+++ cmd.js
@@ -22,10 +22,9 @@ if(cmd == "build") {
     try {	
     	fs.mkdirSync('public/');
     } catch (Exception) {
-        fs.rmdirSync('public/')
+
     }
-    const cwd = process.cwd()
-    fs.copyFileSync(cwd + "/node_modules/raasta-build/server.js", cwd + "/public/server.js");
+    fs.copyFileSync("node_modules/raasta-build/server.js", cwd + "/public/server.js");
 } else if(cmd == "help") {
     console.log("Raasta Build");
     console.log("build - to build project ");
