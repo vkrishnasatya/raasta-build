a467e466869b400601f21d317d4c14b42af0eb22
Krishna Satya <vadapallikrishna@gmail.com>
2020-07-05 14:46:13 +0530

    commiy

--- cmd.js
+++ cmd.js
@@ -7,17 +7,19 @@ const argv = minimist(process.argv.slice(2))
 const cmd = argv._[0]
 
 function createFile(name) {
-  fs.writeFile("public/" + name);
+  fs.writeFile("public/" + name,"Hello, World");
 }
 
 if(cmd == "build") {
     console.log("building");
-    
-    let jsonFile = fs.readFileSync('raasta.json');
-    console.log(jsonFile);
-    fs.mkdir('public/');
-    	
-    	
+    if(!fs.existsSync("raasta.json")) {
+	console.log("JSON file doesnt exists");
+    }
+    let jsonFile = fs.readFileSync('raasta.json','utf8');
+    let json = JSON.parse(jsonFile)
+    console.log(json);
+    fs.mkdirSync('public/');
+	
 } else if(cmd == "help") {
     console.log("Raasta Build");
     console.log("build - to build project ");
