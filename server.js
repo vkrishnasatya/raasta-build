9e77f3e2e8c208014b3d2828abab8a9ab9b45531
Krishna Satya <vadapallikrishna@gmail.com>
2020-07-06 16:27:53 +0530

    m

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 #! /usr/bin/env node
 const express = require('express');
 const raasta = require('raasta-build');
+const hbs = require('hbs');
 const app = express();
 
 const config = raasta.getConfig();
 
+app.set('view engine', 'hbs');
 app.use(express.static('static'));
 
 for(page in config.pages) {
     app.get(config.pages[page].path, function(req,res){
-        res.send("Hello, World");
+        res.render(config.pages[page].template);  
     });
 }
 
